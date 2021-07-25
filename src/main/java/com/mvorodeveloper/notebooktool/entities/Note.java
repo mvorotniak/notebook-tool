c97c422d41b9caa2ca29332e16f50fe1ab14c8c6
mvorotniak <mvorodeveloper@gmail.com>
2021-07-25 06:55:58 -0700

    added edit page and made minimal corrections

--- src/main/java/com/mvorodeveloper/notebooktool/entities/Note.java
+++ src/main/java/com/mvorodeveloper/notebooktool/entities/Note.java
@@ -8,6 +8,7 @@ import java.util.Date;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
@@ -19,7 +20,7 @@ import javax.persistence.Table;
 public class Note {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
     private String message;
